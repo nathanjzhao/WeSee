493f9e8bc50fade592c799653bda04ae9e4b932d
nathanjzhao <43712744+nathanjzhao@users.noreply.github.com>
2020-11-26 23:19:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/wesee/MainActivity.java
@@ -0,0 +1,277 @@
+package com.example.wesee;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Handler;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.graphics.Bitmap;
+import android.widget.Toast;
+
+import org.pytorch.Tensor;
+import org.pytorch.Module;
+import org.pytorch.IValue;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+
+    Module model;
+    private final String TAG = "MyActivity";
+    public TextToSpeech tts;
+
+    TextView mTextView;
+
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
+    Camera mCamera;
+    boolean mPreviewRunning;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!hasCameraPermission()) {
+            requestCameraPermission();
+        }
+
+        mTextView = (TextView)findViewById(R.id.text);
+
+        //btncapture = findViewById(R.id.btncapture);
+        mSurfaceView = findViewById(R.id.surface_camera);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        Log.d(TAG,assetFilePath(this,"mobilenet-v2.pt"));
+        model = Module.load(assetFilePath(this,"mobilenet-v2.pt"));
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int ttsLang = tts.setLanguage(Locale.US);
+
+                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "The Language is not supported!");
+                    } else {
+                        Log.i("TTS", "Language Supported.");
+                    }
+                    Log.i("TTS", "Initialization success.");
+                } else {
+                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP) && mPreviewRunning == true){
+            mCamera.takePicture(null, null, mPictureCallback);
+            mPreviewRunning = false;
+        }
+        return true;
+    }
+
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] imageData, Camera c) {
+            Log.d(TAG,"Picture Taken");
+            Bitmap bitmap = BitmapFactory.decodeByteArray(imageData , 0, imageData.length);
+
+            String temp = predict(bitmap);
+            String pred = capitalizeWord(temp.split(", ")[0]);
+
+            int speechStatus = tts.speak(pred, TextToSpeech.QUEUE_FLUSH, null);
+
+            if (speechStatus == TextToSpeech.ERROR) {
+                Log.e("TTS", "Error in converting Text to Speech!");
+            }
+
+            mTextView.setText(pred);    
+
+            bitmap.recycle();
+            mCamera.startPreview();
+            mPreviewRunning = true;
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+    }
+
+    public static String capitalizeWord(String str){
+        String words[]=str.split("\\s");
+        String capitalizeWord="";
+        for(String w:words){
+            String first=w.substring(0,1);
+            String afterfirst=w.substring(1);
+            capitalizeWord+=first.toUpperCase()+afterfirst+" ";
+        }
+        return capitalizeWord.trim();
+    }
+
+    float[] mean = {0.485f, 0.456f, 0.406f};
+    float[] std = {0.229f, 0.224f, 0.225f};
+
+    public Tensor preprocess(Bitmap bitmap, int size){
+
+        bitmap = Bitmap.createScaledBitmap(bitmap,size,size,false);
+        return TensorImageUtils.bitmapToFloat32Tensor(bitmap,mean,std);
+
+    }
+
+    public int argMax(float[] inputs){
+
+        int maxIndex = -1;
+        float maxvalue = 0.0f;
+
+        for (int i = 0; i < inputs.length; i++){
+
+            if(inputs[i] > maxvalue) {
+
+                maxIndex = i;
+                maxvalue = inputs[i];
+            }
+
+        }
+
+
+        return maxIndex;
+    }
+
+    public String predict(Bitmap bitmap){
+
+        Tensor tensor = preprocess(bitmap,224);
+
+        IValue inputs = IValue.from(tensor);
+        Tensor outputs = model.forward(inputs).toTensor();
+        float[] scores = outputs.getDataAsFloatArray();
+
+        int classIndex = argMax(scores);
+
+        return Constants.IMAGENET_CLASSES[classIndex];
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+        } catch (Exception e) {
+            Log.e(getString(R.string.app_name), "failed to open Camera");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        if (!mPreviewRunning) {
+            mCamera.stopPreview();
+        }
+        Camera.Parameters p = mCamera.getParameters();
+        List<Camera.Size> previewSizes = p.getSupportedPreviewSizes();
+
+        Camera.Size previewSize = previewSizes.get(0);
+        p.setPreviewSize(previewSize.width, previewSize.height);
+
+        mCamera.setParameters(p);
+
+        mCamera.setDisplayOrientation(90);
+
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mCamera.startPreview();
+        mPreviewRunning = true;
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mCamera.stopPreview();
+        mPreviewRunning = false;
+        mCamera.release();
+
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestCameraPermission() {
+        requestPermissions(new String[]{Manifest.permission.CAMERA},
+                100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 100:
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public static String assetFilePath(Context context, String assetName) {
+        File file = new File(context.getFilesDir(), assetName);
+
+        try (InputStream is = context.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        } catch (IOException e) {
+            Log.e("pytorchandroid", "Error process asset " + assetName + " to file path");
+        }
+        return null;
+    }
+}
